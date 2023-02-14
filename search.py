11c9e21bd7d33215b801a3dd636a08cedf020f56
Marco Bocchetti <marcobocchetti3@gmail.com>
2023-02-14 20:40:54 +0100

    tests fixed

--- search.py
+++ search.py
@@ -15,20 +15,39 @@ class bcolors:
 
 parser = argparse.ArgumentParser(description="Just an example",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
 parser.add_argument("-k", default=None, type=int)
+parser.add_argument("-c", "--cutoff",  default=0.0, type=float)
+parser.add_argument("-s", "--stemming",  default=True, type=bool)
+parser.add_argument("-r", "--remove_stopwords",  default=True, type=bool)
+parser.add_argument("-x", "--expand_synonyms",  default=False, type=bool)
+parser.add_argument("--similarity",
+                    choices=[SearchEngineConfig.COSINE,
+                             SearchEngineConfig.DOT_PRODUCT,
+                             SearchEngineConfig.EUCLIDEAN],
+                    type=str.upper,
+                    default=SearchEngineConfig.COSINE)
 
 args = parser.parse_args()
 k = args.k
+cutoff = args.cutoff
+stemming = args.stemming
+remove_stopwords = args.remove_stopwords
+expand_synonyms = args.expand_synonyms
+similarity = args.similarity
 
 searchEngine = SearchEnginePrototype(SearchEngineConfig(
         config_name="Prototype_Search_Engine",
-        expand_synonyms=False,
-        remove_stopwords=True,
-        cutoff=0.0,
-        similarity=SearchEngineConfig.EUCLIDEAN,
-        use_stemming=True
+        expand_synonyms=expand_synonyms,
+        remove_stopwords=remove_stopwords,
+        cutoff=cutoff,
+        similarity=similarity,
+        use_stemming=stemming
     )
 )
+
+print("Initialized search engine with config:")
+print(json.dumps(searchEngine.config.to_dict(), indent=4))
 
 while True:
     user_input = input("{}ENTER QUERY:{} ".format(bcolors.GREEN, bcolors.INFO))
@@ -39,6 +58,9 @@ while True:
 
     all_results_with_score = searchEngine.search(query)
     all_results = [r["document"] for r in all_results_with_score]
+
+    if k and len(all_results) > k:
+        print("{}(Showing the {} most relevant results out of {}){}".format(bcolors.YELLOW, k, len(all_results), bcolors.INFO))
 
     results = all_results if k is None else all_results[:min(k, len(all_results))]
     print("{}\n".format(json.dumps(results, indent=4)))
